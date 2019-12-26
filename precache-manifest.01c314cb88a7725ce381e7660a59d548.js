b9637e309767bb4c0537d31a68ec1d53379f3b4d
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-26 19:37:56 +0200

    Updates

---
+++ precache-manifest.01c314cb88a7725ce381e7660a59d548.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b54efe525098a1aeadcb5b7c2342d7",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "ddfa198b0afb08b04348",
+    "url": "/react_tabs-with-router/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "25a075ac788fca0c6b66",
+    "url": "/react_tabs-with-router/static/js/2.5e8ea60e.chunk.js"
+  },
+  {
+    "revision": "ddfa198b0afb08b04348",
+    "url": "/react_tabs-with-router/static/js/main.44ed3550.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
